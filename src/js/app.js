34a930fefef2eeb1795d4d68a2aa159e039f2f0d
potedev <quentin@pote.dev>
2021-05-17 11:45:53 +0200

    [Redux] Added Redux as our state manager along with a root reducer

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-
-//Our parents containers
-import Login from './pages/login'
-import Home from './pages/home'
-
 const App = () => {
     const dispatch = useDispatch();
     const appState = useSelector(state => state.app)
@@ -22,26 +16,7 @@ const App = () => {
 
     console.log('APP global state : ', appState)
 
-    if (appState.loading) return <div>Loading...</div>
-
-    return (
-        <Router>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/login" component={Login} />
-            </Switch>
-        </Router>
-    )
+    return <h1>Hello App</h1>
 }
 
 export default App
